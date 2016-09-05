6e62106b27ac009667bae3d8dacbdb8c6f4f44a5
Rajesh Koyi <285258343+koyirajesh97-ai@users.noreply.github.com>
2016-09-05 15:12:28 +0000

    decimal to binary

---
+++ src/DecimalToBinary.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String bin = "";
+        int num = n;
+        if(num == 0) bin = "0";
+        while(num > 0){
+            bin = (num % 2) + bin;
+            num = num / 2;
+        }
+        System.out.println("binary: " + bin);
+        sc.close();
+    }
+}
