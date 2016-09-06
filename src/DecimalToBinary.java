15f9d5b2be7e1f3a91dc1cc74113f793537349a7
Rajesh Koyi <285258343+koyirajesh97-ai@users.noreply.github.com>
2016-09-06 16:40:46 +0000

    minor edit

--- src/DecimalToBinary.java
+++ src/DecimalToBinary.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class DecimalToBinary {
     public static void main(String[] args){
+    // done
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String bin = "";
